c9991e0207e13f993bc7ced4a8fddb19743f69ae
asxe <2973918177@qq.com>
2023-10-20 13:53:14 +0800

    优化体验

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ function getImages() {
     let indexName = "image-index";
     let index = sessionStorage.getItem(indexName);
     let panel = document.querySelector('#panel');
-    if (isNaN(parseInt(index,10)) || index == 7) index = 0;
+    if (isNaN(parseInt(index, 10)) || index == 7) index = 0;
     else index++;
     panel.style.background = "url('https://api.asxe.vip/scenery.php') center center no-repeat #666";
     panel.style.backgroundSize = "cover";
@@ -55,17 +55,55 @@ function ShowRunTime(id) {
         u = n + " 天 " + a + " 时 " + r + " 分 " + h + " 秒";
     document.getElementById(id).textContent = "本站已运行 " + u
 }
+
 setInterval("ShowRunTime('RunTime')", 1e3)
 
+
+function description(r, text) {
+    function t() {
+        return b[Math.floor(Math.random() * b.length)];
+    }
+
+    function e() {
+        return String.fromCharCode(94 * Math.random() + 33);
+    }
+
+    function n(r) {
+        for (let n = document.createDocumentFragment(), i = 0; r > i; i++) {
+            let l = document.createElement("span");
+            l.textContent = e();
+            l.style.color = t();
+            n.appendChild(l);
+        }
+        return n;
+    }
+
+    function i() {
+        let t = o[c.skillI];
+        c.step ? c.step-- : ((c.step = g), c.prefixP < l.length ? (c.prefixP >= 0 && (c.text += l[c.prefixP]), c.prefixP++) : "forward" === c.direction ? c.skillP < t.length ? ((c.text += t[c.skillP]), c.skillP++) : c.delay ? c.delay-- : ((c.direction = "backward"), (c.delay = a)) : c.skillP > 0 ? ((c.text = c.text.slice(0, -1)), c.skillP--) : ((c.skillI = (c.skillI + 1) % o.length), (c.direction = "forward"))), (r.textContent = c.text), r.appendChild(n(c.prefixP < l.length ? Math.min(s, s + c.prefixP) : Math.min(s, t.length - c.skillP))), setTimeout(i, d);
+    }
+
+    let l = "",
+        o = [text].map(function (r) {
+            return r + "";
+        }),
+        a = 3,
+        g = 1,
+        s = 5,
+        d = 75,
+        b = ["rgb(110,64,170)", "rgb(150,61,179)", "rgb(191,60,175)", "rgb(228,65,157)", "rgb(254,75,131)", "rgb(255,94,99)", "rgb(255,120,71)", "rgb(251,150,51)", "rgb(226,183,47)", "rgb(198,214,60)", "rgb(175,240,91)", "rgb(127,246,88)", "rgb(82,246,103)", "rgb(48,239,130)", "rgb(29,223,163)", "rgb(26,199,194)", "rgb(35,171,216)", "rgb(54,140,225)", "rgb(76,110,219)", "rgb(96,84,200)"],
+        c = {text: "", prefixP: -s, skillI: 0, skillP: 0, direction: "forward", delay: a, step: g};
+    i();
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             let res = JSON.parse(this.responseText);
-            document.getElementById('description').innerHTML = res['hitokoto'] + "<br/> 「<strong>" + res['from'] + "</strong>」";
+            description(document.getElementById('description'), res['hitokoto']);
         }
-    };
+    }
     xhr.open("GET", "https://v1.hitokoto.cn", true);
     xhr.send();
 
@@ -83,25 +121,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('contextmenu', (evt) => {
     evt.preventDefault();
-    // let div = document.createElement("div");
-    // let span = document.createElement("span");
-    // let header = document.getElementById('panel');
-    // span.textContent = '为了浏览体验，本站禁用右键';
-    // div.appendChild(span);
-    // div.style.top = '15px';
-    // div.style.display = 'flex';
-    // div.style.width = 'auto+10px';
-    // div.style.height = '40px';
-    // div.style.position = 'fixed';
-    // div.style.alignItems = 'center';
-    // div.style.justifyContent = 'center';
-    // div.style.backgroundColor = 'black';
-    // span.style.color = 'red';
-    //
-    // header.appendChild(div);
-    // setTimeout(() => {
-    //     div.remove();
-    // },2000);
 });
 
 function msg() {
@@ -115,7 +134,7 @@ function msg() {
         " /_/    \\_\\_____//_/ \\_\\______|\n" +
         "                               \n" +
         "                               \n"
-    ,'color:red');
+        , 'color:red');
     console.log('%cASXE的主页\nhttps://www.asxe.vip', 'color:green');
 
 }
